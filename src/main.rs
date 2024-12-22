cd01de72348e44142e2ab607be9d7e8453624928
shina <53410646+s7tya@users.noreply.github.com>
2024-12-23 03:32:16 +0900

    トークン間の空白を許容するためにトークナイザを追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use core::panic;
-use std::{env::args, str::FromStr};
+use std::env::args;
+
+#[derive(PartialEq, Eq)]
+enum TokenKind {
+    Reserved,
+    Num(i32),
+    Eof,
+}
+
+struct Token<'src> {
+    kind: TokenKind,
+    raw_str: &'src str,
+}
 
 fn main() {
     let args = args().collect::<Vec<_>>();
@@ -7,52 +19,126 @@ fn main() {
         panic!("引数の個数が正しくありません");
     }
 
-    let mut sc = Scanner::new(&args[1]);
+    let mut lexer = Lexer::new(&args[1]);
+    let tokens = lexer.lex();
+    let mut parser = Parser::new(tokens);
 
     println!("  .globl main");
     println!("main:");
-    println!("  li a0, {}", sc.str_to_fromstr::<i32>().unwrap());
+    println!("  li a0, {}", parser.expect_number());
 
-    while let Some(c) = sc.source.chars().nth(sc.cursor) {
-        if c == '+' {
-            sc.cursor += 1;
-            println!("  addi a0, a0, {}", sc.str_to_fromstr::<i32>().unwrap());
+    while !parser.at_eof() {
+        if parser.consume("+") {
+            println!("  addi a0, a0, {}", parser.expect_number());
             continue;
         }
 
-        if c == '-' {
-            sc.cursor += 1;
-            println!("  addi a0, a0, -{}", sc.str_to_fromstr::<i32>().unwrap());
-            continue;
-        }
-
-        panic!(
-            "予期しない文字です: '{}'",
-            sc.source.chars().nth(sc.cursor).unwrap()
-        );
+        parser.expect("-");
+        println!("  addi a0, a0, -{}", parser.expect_number());
     }
 
     println!("  ret");
 }
 
-struct Scanner<'src> {
-    pub source: &'src str,
-    pub cursor: usize,
+struct Parser<'src> {
+    tokens: Vec<Token<'src>>,
+    cursor: usize,
 }
 
-impl<'src> Scanner<'src> {
+impl<'src> Parser<'src> {
+    pub fn new(tokens: Vec<Token<'src>>) -> Self {
+        Self { tokens, cursor: 0 }
+    }
+
+    pub fn consume(&mut self, op: &str) -> bool {
+        let token = &self.tokens[self.cursor];
+        if token.kind != TokenKind::Reserved || token.raw_str != op {
+            return false;
+        }
+        self.cursor += 1;
+
+        true
+    }
+
+    pub fn expect(&mut self, op: &str) {
+        let token = &self.tokens[self.cursor];
+        if token.kind != TokenKind::Reserved || token.raw_str != op {
+            panic!("'{}' ではありません", op);
+        }
+        self.cursor += 1;
+    }
+
+    pub fn expect_number(&mut self) -> i32 {
+        let token = &self.tokens[self.cursor];
+        if let TokenKind::Num(value) = token.kind {
+            self.cursor += 1;
+            value
+        } else {
+            panic!("数ではありません");
+        }
+    }
+
+    pub fn at_eof(&self) -> bool {
+        self.tokens[self.cursor].kind == TokenKind::Eof
+    }
+}
+
+struct Lexer<'src> {
+    source: &'src str,
+    cursor: usize,
+}
+
+impl<'src> Lexer<'src> {
     pub fn new(source: &'src str) -> Self {
         Self { source, cursor: 0 }
     }
 
-    pub fn str_to_fromstr<F: FromStr>(&mut self) -> Result<F, F::Err> {
-        let source_chars = self.source.chars().collect::<Vec<_>>();
-        let start = self.cursor;
-        while self.cursor < self.source.len() && source_chars[self.cursor].is_ascii_digit() {
-            self.cursor += 1;
+    pub fn lex(&mut self) -> Vec<Token<'src>> {
+        let mut tokens = vec![];
+
+        while self.cursor < self.source.len() {
+            let c = self.source[self.cursor..].chars().next().unwrap();
+
+            if c.is_whitespace() {
+                self.cursor += 1;
+                continue;
+            }
+
+            if c == '+' || c == '-' {
+                tokens.push(Token {
+                    kind: TokenKind::Reserved,
+                    raw_str: &self.source[self.cursor..=self.cursor],
+                });
+                self.cursor += 1;
+                continue;
+            }
+
+            if c.is_ascii_digit() {
+                let start = self.cursor;
+                while self.cursor < self.source.len()
+                    && self.source[self.cursor..]
+                        .chars()
+                        .next()
+                        .unwrap()
+                        .is_ascii_digit()
+                {
+                    self.cursor += 1;
+                }
+                tokens.push(Token {
+                    kind: TokenKind::Num(self.source[start..self.cursor].parse::<i32>().unwrap()),
+                    raw_str: &self.source[start..self.cursor],
+                });
+                continue;
+            }
+
+            panic!("トークナイズできません");
         }
 
-        let token = &self.source[start..self.cursor];
-        token.parse()
+        tokens.push(Token {
+            kind: TokenKind::Eof,
+            raw_str: "",
+        });
+
+        tokens
     }
 }
